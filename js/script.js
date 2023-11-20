647634e21b3334787132c6fe347c0c508d087c23
adieptr <adie6809@gmail.com>
2023-11-20 12:49:20 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const navbarNav = document.querySelector('.navbar-nav');
+document.querySelector('#ham-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+const ham = document.querySelector('#ham-menu');
+document.addEventListener('click', function(e){
+    if(!ham.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+})
